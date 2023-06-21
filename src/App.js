479eabd522f5bd59b822c584aa4c0a5fd41b72ef
NguyenMinhChien <minhchien03112003@gmail.com>
2023-06-22 01:37:06 +0700

    useEffect #2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 //import State from './useState'
-import TwoWayBinding from './TwoWayBinding';
+// import TwoWayBinding from './TwoWayBinding';\
+import Effect from './useEffect';
+
+import {useState} from 'react'
+
 
 function App() {
+  const [show,setShow] = useState(false)
   return (
     <div className="wrapper" style={{padding: '30px'}}>
       <div>This is Home Page</div>
-      <TwoWayBinding></TwoWayBinding>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <Effect></Effect>}
     </div>
   );
 }
--- src/useEffect/index.js
+++ src/useEffect/index.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
+const tabs = ['posts','comments','albums'];
 
 //1. useEffect(callback)
 //  - callback duoc goi khi component re-render
 //  - callback duoc goi sau khi elements da duoc them vao DOM    
 //2. useEffect(callback,[])
 //  - callback chi duoc goi mot lan khi component mounted
-//3. useEffect(callback,[depens])
+//3. useEffect(callback,[deps])
+//  -callback duoc goi khi deps thay doi
 
 
 //------------- DUNG CHUNG CUA EFFECT ----------------
@@ -13,11 +15,15 @@ import { useEffect, useState } from "react";
 function Effect() {
     const [title,setTitle] = useState('');
     const [post,setPost] = useState([]);
+    const [type,setType] = useState('posts');
+    console.log(type);
 
+    //1.
     useEffect(() => {
         document.title = title;
     })
 
+    //2.
     useEffect(()=> {
         fetch("https://jsonplaceholder.typicode.com/posts")
             .then(res => res.json())
@@ -25,6 +31,15 @@ function Effect() {
                 setPost(data)
             })
     },[])
+
+    //3. 
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
+        .then(res => res.json())
+        .then(data => {
+            setPost(data)
+        })
+    },[type]);
 
 
     return ( 
@@ -35,9 +50,18 @@ function Effect() {
                 type="text" 
                 style={{marginTop: '10px'}}
             />
+            {tabs.map((tab) => {
+                return <button  
+                    style={type === tab ? {background: '#333', color: '#fff'} : {}} 
+                    key={tab} 
+                    onClick={() => setType(tab)}
+                >
+                    {tab}
+                </button>
+            })}
             <ul>
                 {post.map((item) => {
-                    return <li key={item.id}>{item.title}</li>
+                    return <li key={item.id}>{item.title || item.email}</li>
                 })}
             </ul>
         </div>
